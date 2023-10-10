152b28eb4799260d3e32d9430637265435651419
sugusu <113091592+sugusu@users.noreply.github.com>
2023-10-10 08:59:18 +0000

    20

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,12 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice =[a.get(1).unwrap(),a.get(2).unwrap(),a.get(3).unwrap()];// TODO: Finish this line so that the slice is a slice of the second & third elements of the array
+    let nice_slice =&a[1..4];//这里的nice_slice是一个切片，切片是一个指向数组的指针，指向数组的某一部分，这里的nice_slice指向a数组的第二个元素到第四个元素
 
 
     assert_eq!([2, 3, 4], nice_slice)//assert_eq!函数是断言函数，如果两个参数不相等，就会报错
