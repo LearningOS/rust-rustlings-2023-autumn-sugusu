17ede2bc7344f428471db76dad6f478e303dce55
sugusu <113091592+sugusu@users.noreply.github.com>
2023-10-10 08:54:04 +0000

    19

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -11,7 +11,8 @@
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice =[a.get(1).unwrap(),a.get(2).unwrap(),a.get(3).unwrap()];// TODO: Finish this line so that the slice is a slice of the second & third elements of the array
 
-    assert_eq!([2, 3, 4], nice_slice)//
+
+    assert_eq!([2, 3, 4], nice_slice)//assert_eq!函数是断言函数，如果两个参数不相等，就会报错
 }
