f98f1d66cbe0419b9b44c1d2de312655611e42e5
sugusu <113091592+sugusu@users.noreply.github.com>
2023-10-11 14:18:48 +0000

    44

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -8,10 +8,10 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // TODO: Complete this use statement
-use ???
+use std::time::{SystemTime, UNIX_EPOCH};//这一步为了让下面的SystemTime和UNIX_EPOCH可以直接使用
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
