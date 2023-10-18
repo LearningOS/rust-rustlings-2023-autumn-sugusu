7811b993d9a27090168bbc25007af70de7183af1
sugusu <113091592+sugusu@users.noreply.github.com>
2023-10-18 06:39:38 +0000

    100

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -1,3 +1,13 @@
+// tests6.rs
+//
+// In this example we take a shallow dive into the Rust standard library's
+// unsafe functions. Fix all the question marks and todos to make the test
+// pass.
+//
+// Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
+// hint.
+
+
 struct Foo {
     a: u128,
     b: Option<String>,
@@ -7,9 +17,13 @@ struct Foo {
 ///
 /// The `ptr` must contain an owned box of `Foo`.
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
-    // SAFETY: The `ptr` contains an owned box of `Foo by contract. We
+    // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    Box::from_raw(ptr)
+    let mut ret: Box<Foo> = unsafe { 
+        Box::from_raw(ptr)
+     };
+    ret.b = Some("hello".to_owned());
+    ret
 }
 
 #[cfg(test)]
@@ -19,7 +33,7 @@ mod tests {
 
     #[test]
     fn test_success() {
-        let data = Box::new(Foo { a: 1, b: Some("hello".to_owned()) });
+        let data = Box::new(Foo { a: 1, b: None });
 
         let ptr_1 = &data.a as *const u128 as usize;
         // SAFETY: We pass an owned box of `Foo`.
